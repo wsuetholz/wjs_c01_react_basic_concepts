0ccc35a29cae92ac00adc9fbe0b20747fc7eabcd
William Suetholz <bills@mwes.com>
2022-01-19 19:31:53 -0600

    Modifications to use mui material design elements.

--- src/components/country.jsx
+++ src/components/country.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import logo from "../logo.svg";
+import ReactDOM from 'react-dom';
+import {grey} from "@mui/material/colors";
+import {Avatar, Box, Button, Card, Icon, Stack, Typography, Divider} from "@mui/material";
+import {Add} from "@mui/icons-material";
 
 class Country extends Component {
     state = {
         name: 'Nauru',
+        capitol: 'Yaren District',
         flag: 'http://worldometers.info/img/flags/nr-flag.gif',
         gold: 0,
     }
@@ -14,24 +18,30 @@ class Country extends Component {
 
     render() {
         return (
-            < div className="Country" >
-                <div>
-                    Country:
-                    <a className='CountryName' >
-                        { this.state.name }
-                    </a>
-                    <img src={this.state.flag} className="CountryFlag" alt="Flag" />
-                </div>
-                <div>
-                    Current Number of Gold Medals:
-                    <a className='MedalCount' >
-                        { this.state.gold }
-                    </a>
-                </div>
-                <button onClick={ this.handleClick } className='IncrMedalButton' >
-                    Increment Gold Medal Count
-                </button>
-            </div>
+            <Card className="Country" align="center" >
+                <Box sx={{ p: 2, display: 'flex' }}>
+                    <Avatar variant="rounded" src={this.state.flag} className="CountryFlag"  />
+                    <Stack spacing={0.5} alignItems="left" className="CountryName">
+                        <Typography fontWeight={700}>{this.state.name}</Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {this.state.capitol}
+                        </Typography>
+                    </Stack>
+                </Box>
+                <Divider />
+                <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ px: 2, py: 1, bgcolor: 'background.default' }}>
+                    <Stack direction="column" alignItems="center" justifyContent="space-between">
+                        <Avatar className='MedalCount' >
+                            {this.state.gold}
+                        </Avatar>
+                        <Typography fontWeight={400}>Gold Medals</Typography>
+                    </Stack>
+                    <Divider />
+                    <Button variant="contained" className='IncrMedalButtom' onClick={ this.handleClick } >
+                        <Icon><Add fontSize='medium' /></Icon>Increment Medal Count
+                    </Button>
+                </Stack>
+            </Card>
         );
     }
 }
